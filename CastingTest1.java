7ec90a05cb02aa17540456bfd721530f19fca33c
poyoJGY <104569930+poyoJGY@users.noreply.github.com>
2022-12-18 22:03:40 +0900

    Add files via upload

---
+++ CastingTest1.java
@@ -0,0 +1,36 @@
+
+public class CastingTest1 {
+
+	public static void main(String[] args) {
+
+		Car car = null;
+		FireEngine fe = new FireEngine();
+		FireEngine fe2 = null;
+		
+		fe.water();
+		car = fe;		// car = (Car)fe;
+//		car.water();	// error
+		fe2 = (FireEngine)car;
+		fe.water();
+		
+	}
+
+}
+
+class Car {
+	String color;
+	int door;
+	
+	void drive() {
+		System.out.println("drive, Brrrr~");
+	}
+	
+	void stop() {
+		System.out.println("stop!!!");
+	}
+}
+class FireEngine extends Car {
+	void water() {
+		System.out.println("water!");
+	}
+}
