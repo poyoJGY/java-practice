1d1f865440f765511c72cc22ca4abd95c2f692fc
poyoJGY <104569930+poyoJGY@users.noreply.github.com>
2023-01-06 19:49:08 +0900

    Add files via upload

---
+++ Exception_Handling/ChainedExceptionEx.java
@@ -0,0 +1,72 @@
+package ch8;
+
+public class ChainedExceptionEx {
+
+	public static void main(String[] args) {
+
+		try {
+			install();
+		} catch(InstallException e) {
+			e.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}	// main 끝
+	
+	static void install() throws InstallException {
+		try {
+			startInstall();
+			copyFiles();
+		} catch(SpaceException se) {
+			InstallException ie = new InstallException("설치 중 예외 발생");
+			ie.initCause(se);
+		} catch(MemoryException me) {
+			InstallException ie = new InstallException("설치 중 예외 발생");
+			ie.initCause(me);
+			throw ie;
+		} finally {
+			deleteTempFiles();
+		}	// try의 끝
+	}
+	
+	static void startInstall() throws SpaceException, MemoryException {
+		if(!enoughSpace()) {
+			throw new SpaceException("설치할 공간이 부족합니다.");
+		}
+		
+		if(!enoughMemory()) {
+			throw new MemoryException("메모리가 부족합니다.");
+//			throw new RuntimeException(new MemoryException("메모리가 부족합니다."));
+		}
+	}	// startInstall 메서드의 끝
+	
+	static void copyFiles() {/* 파일들을 복사하는 코드를 적는다. */}
+	static void deleteTempFiles() {/* 임시파일들을 삭제하는 코드를 적는다. */}
+	
+	static boolean enoughSpace() {
+		return false;
+	}
+	
+	static boolean enoughMemory() {
+		return true;
+	}	
+}	// ChainedExceptionEx 클래스의 끝
+
+class InstallException extends Exception {
+	InstallException(String msg) {
+		super(msg);
+	}
+}
+
+class SpaceException extends Exception {
+	SpaceException(String msg) {
+		super(msg);
+	}
+}
+
+class MemoryException extends Exception {
+	MemoryException(String msg) {
+		super(msg);
+	}
+}
